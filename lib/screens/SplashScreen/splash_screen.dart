b59780a2bb653fdbd25657f563c9c0a5b47920ea
efrainlaycastillo <efrainlaycastillo@hotmail.com>
2021-01-30 14:36:24 -0500

    Spash screen

--- lib/screens/SplashScreen/splash_screen.dart
+++ lib/screens/SplashScreen/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 class SplashPage extends StatelessWidget {
   static Route route() {
@@ -7,8 +8,31 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: CircularProgressIndicator()),
+    return SplashScreen(
+        seconds: 14,
+        navigateAfterSeconds: new AfterSplash(),
+        title: new Text('Welcome In SplashScreen'),
+        image: new Image.asset('icon-praemi.png'),
+        backgroundColor: Colors.white,
+        styleTextUnderTheLoader: new TextStyle(),
+        photoSize: 100.0,
+        loaderColor: Colors.red);
+  }
+}
+
+class AfterSplash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+          title: new Text("Welcome In SplashScreen Package"),
+          automaticallyImplyLeading: false),
+      body: new Center(
+        child: new Text(
+          "Done!",
+          style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 30.0),
+        ),
+      ),
     );
   }
 }
