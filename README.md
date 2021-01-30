3c09e5dc776481fcb281d45acbbeebdda3505c6d
efrainlaycastillo <efrainlaycastillo@hotmail.com>
2021-01-29 22:38:47 -0500

    queries

--- README.md
+++ README.md
@@ -10,12 +10,12 @@ A few resources to get you started if this is your first Flutter project:
 
 - [WooCommerce](http://praemi.3.94.78.53.xip.io/)
 - User#1
-  - User: restapi
+  - User: restapi - ADMIN
   - Password: #Panama01
-- User#2
+- User#2 - VENDOR
   - User: JohnSmith
   - Password: Password01
-- User#3
+- User#3 - ADMIN
   - User: efrainlay
   - Password: 2021Pass#/*-
 
--- lib/services/WordpressAPI.dart
+++ lib/services/WordpressAPI.dart
@@ -1,8 +1,11 @@
 import 'dart:developer';
 import 'dart:convert';
+import 'dart:math';
 
 import 'package:flutter_session/flutter_session.dart';
 import 'package:http/http.dart' as http;
+import 'package:praemiclient/models/models.dart';
+import 'package:praemiclient/repositories/WooPraemi.dart';
 
 class WordpressAPI {
   static const String baseURL = "http://praemi.3.94.78.53.xip.io/wp-json";
@@ -30,7 +33,6 @@ class WordpressAPI {
 
   Future<Map<String, dynamic>> queryGetUser() async {
     dynamic sessionToken = await FlutterSession().get("tokens");
-    print(sessionToken["token"]);
     var res = await http.post("$baseURL/wp/v2/users/me", headers: {
       'Content-type': 'application/json',
       'Accept': 'application/json',
@@ -39,5 +41,87 @@ class WordpressAPI {
     Map<String, dynamic> jsonResponse = json.decode(res.body);
     print(jsonResponse["username"]);
     return jsonResponse;
+  }
+
+  Future<Map<String, dynamic>> queryMakeOrder(
+      {User userData, int productID}) async {
+    Random rand = Random();
+    List<int> codeUnits = List.generate(10, (index) {
+      return rand.nextInt(26) + 97;
+    });
+    String nonce = String.fromCharCodes(codeUnits);
+    int timestamp = DateTime.now().millisecondsSinceEpoch ~/ 1000;
+
+    dynamic sessionToken = await FlutterSession().get("tokens");
+    String parameters = "oauth_consumer_key=" +
+        WooPraemi.consumerKeyConst +
+        "&oauth_nonce=" +
+        nonce +
+        "&oauth_signature_method=HMAC-SHA1&oauth_timestamp=" +
+        timestamp.toString() +
+        "&oauth_token=" +
+        sessionToken["token"] +
+        "&oauth_version=1.0&";
+    var _headers = {
+      'Authorization': '$parameters',
+      'Content-Type': 'application/json'
+    };
+    var _body = {
+      "payment_method_title": "Transferencia bancaria directa",
+      "set_paid": true,
+      "status": "completed",
+      "customer_id": userData.idUser,
+      "billing": {
+        "first_name": userData.username,
+        "country": "PTY",
+        "email": userData.email
+      },
+      "line_items": [
+        {"product_id": productID, "quantity": 1}
+      ]
+    };
+    var res = await http.post("$baseURL/wp-json/wc/v3/orders",
+        headers: _headers, body: _body);
+
+    if (res.statusCode == 201) {
+      Map<String, dynamic> jsonResponse = json.decode(res.body);
+      return jsonResponse;
+    } else {
+      return null;
+    }
+  }
+
+  Future<Map<String, dynamic>> queryGetOrderByVendor({User userData}) async {
+    Random rand = Random();
+    List<int> codeUnits = List.generate(10, (index) {
+      return rand.nextInt(26) + 97;
+    });
+    String nonce = String.fromCharCodes(codeUnits);
+    int timestamp = DateTime.now().millisecondsSinceEpoch ~/ 1000;
+
+    dynamic sessionToken = await FlutterSession().get("tokens");
+    String parameters = "oauth_consumer_key=" +
+        WooPraemi.consumerKeyConst +
+        "&oauth_nonce=" +
+        nonce +
+        "&oauth_signature_method=HMAC-SHA1&oauth_timestamp=" +
+        timestamp.toString() +
+        "&oauth_token=" +
+        sessionToken["token"] +
+        "&oauth_version=1.0&";
+    var _headers = {
+      'Authorization': '$parameters',
+      'Content-Type': 'application/json'
+    };
+    var res = await http.get(
+        "$baseURL/wp-json/wc/v2/orders/?vendor=${userData.idUser}",
+        headers: _headers);
+
+    if (res.statusCode == 201) {
+      Map<String, dynamic> jsonResponse = json.decode(res.body);
+      return jsonResponse;
+    } else {
+      return null;
+    }
   }
 }
